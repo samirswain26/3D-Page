5fbd99b183095e7e18907aebc753242a56bcd9ab
Samir Kumar Swain <samirkumarswain267@gmail.com>
2025-11-04 23:35:54 +0530

    Header section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Header from "./components/Header";
+
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <img
+        src="/gradient.png"
+        alt="Gradient.img"
+        className="absolute top-0 right-0 opacity-60 -z-10"
+      />
+
+      <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10"></div>
+
+      <Header/>
+    </>
+  );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,90 @@
+import "boxicons/css/boxicons.min.css";
+
+const Header = () => {
+
+  const togglemobileMenu = () => {
+    const mobileMenu = document.getElementById('mobileMenu')
+
+    if(mobileMenu.classList.contains('hidden')){
+      mobileMenu.classList.remove('hidden')
+    }else{
+      mobileMenu.classList.add('hidden')
+    }
+  }
+
+  return (
+    <header className="flex justify-between items-center py-4 px-4 lg:px-20">
+      <h1 className="text-xl md:text-4xl lg:text-5xl font-light m-0">MAIL</h1>
+
+      <nav className="hidden md:flex items-center gap-12 ">
+        <a
+          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+          href="#"
+        >
+          COMPANY
+        </a>
+        <a
+          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+          href="#"
+        >
+          FEATURES
+        </a>
+        <a
+          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+          href="#"
+        >
+          RESOURCES
+        </a>
+        <a
+          className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+          href="#"
+        >
+          DOCS
+        </a>
+      </nav>
+
+      <button
+        className="hidden md:block bg-[#a7a7a7] text-black py-3 px-4 rounded-full border-none font-medium
+        transition-all duration-500 hover:bg-white cursor-pointer z-50
+      "
+      >
+        SIGNIN
+      </button>
+
+      <button className="md:hidden text-3xl p-2 z-50" onClick={togglemobileMenu} >
+        <i class="bx bx-menu"></i>
+      </button>
+
+      <div id="mobileMenu" className="hidden fixed top-16 left-0 right-0 buttom-0 md:hidden z-40 bg-black bg-opacity-70 backdrop-blur-md " > 
+        <nav className="flex flex-col gap-6 items-center" >
+          <a
+            className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+            href="#"
+          >
+            COMPANY
+          </a>
+          <a
+            className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+            href="#"
+          >
+            FEATURES
+          </a>
+          <a
+            className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+            href="#"
+          >
+            RESOURCES
+          </a>
+          <a
+            className="text-base tracking-wider transition-colors hover:text-gray-300 z-50"
+            href="#"
+          >
+            DOCS
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
